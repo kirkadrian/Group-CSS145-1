cce84d20ae7c659c5564b65842e3e6d95b51e8f6
christian-manalang <christianmanalang@gmail.com>
2024-10-07 21:16:30 +0800

    forgot to add the code for 2

--- Group.py
+++ Group.py
@@ -25,3 +25,21 @@ plt.ylabel('Frequency')
 st.pyplot(plt)
 
 #Manalang 2
+
+data = df['TypeName'].value_counts()
+
+custom_order = ['Notebook', 'Gaming', 'Ultrabook', 'Netbook', '2 in 1 Convertible', 'Workstation']
+
+data = data.reindex(custom_order).fillna(0)
+
+color = ['#7AD9F8', '#A8D600', '#F15A5E', '#D46783', '#FFB84D', '#B47FC1']
+
+explode = [0.05] * len(data)
+
+plt.figure(figsize=(10, 8))
+plt.pie(data, labels=data.index, autopct='%1.1f%%', startangle=40, colors=color, explode=explode, shadow=True, wedgeprops={'edgecolor':'black', 'linewidth' : 0.5, 'antialiased' : True})
+
+plt.title('Distribution of Laptop Types', fontsize=12, fontweight='bold')
+
+plt.axis('equal')
+plt.show()
